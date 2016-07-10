0562122ac015595e0768ee2af1592761f179b39e
ardeshir <ardeshir.org@gmail.com>
2016-07-10 17:27:02 +0000

    clearers arrived

--- app.js
+++ app.js
@@ -24,21 +24,10 @@ function Tasks() {
 	// adding Task Element
 
 	function addTaskElement(taskName) {
-		/* var $task = $("<li></li>");
-		var $delete = $("<button class='delete'>X</button>");
-		var $moveUp = $("<button class='move-up'>^</button>");
-		var $moveDown = $("<button class='move-down'>v</button>");
-		$task.append($moveUp)
-		     .append($moveDown)
-                     .append("<span class='task-name'>" + taskName + "</span>")
-		     .append($delete);
-
-		$delete.click(function(){ $task.remove(); }); */
 
 		var $task = $("#task-template .task").clone();
 		$("span.task-name", $task).text(taskName);
                                                              
-		/* $task.text(taskName); */
 		$("#task-list").append($task);
 	        $("button.delete", $task).click(function() {
 			$task.remove();
@@ -50,12 +39,6 @@ function Tasks() {
 			$task.insertAfter($task.next());
 		});
 	
-		/* $moveUp.click(function() {
-			$task.insertBefore($task.prev());
-		});
-		$moveDown.click(function() {
-			$task.insertAfter($task.next());
-		});*/
 
 
 	} // end of task element
@@ -63,7 +46,7 @@ function Tasks() {
 	this.start = function() {
 
 		$("#new-task-name").keypress(function(e) {
-			if(e.which == 13){ // endter key
+			if(e.which == 13){ // enter key is pressed
 				addTask();
 				return false;
 			} // end if 
