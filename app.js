3489fac87b0c35b4020f666830d27a40dbdccf09
ardeshir <ardeshir.org@gmail.com>
2016-07-10 16:00:38 +0000

    adding tasks

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ function Tasks() {
 	function setStatus(message) {
 		$("#app>footer").text(message);
 	} // end of set Status
+	
+	// adding addTask
+
+	function addTask() {
+		var taskName = $("#new-task-name").val();
+
+		if(taskName) {
+			addTaskElement(taskName);
+			// reset test filed
+			$("#new-task-name").val("").focus();
+		} // end of if
+
+
+	} // end of addTask
+
+	// adding Task Element
+
+	function addTaskElement(taskName) {
+		var $task = $("<li></li>");
+		$task.text(taskName);
+		$("task-list").append($task);
+
+	} // end of task element
 
 	this.start = function() {
 
