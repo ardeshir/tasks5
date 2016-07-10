f2bc89fbab48bdf05415958b3266a2cf6dd07534
Ardeshir Sepahsalar <ardeshir.org@gmail.com>
2016-07-10 15:20:13 +0000

    added keypress

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ function Tasks() {
 	} // end of set Status
 
 	this.start = function() {
+
+		$(#new-task-name").keypress(function(e) {
+			if(e.which == 13){ // endter key
+				addTask();
+				return false;
+			} // end if 
+                                         
+		}).focus();
+
 		$("#app>header").append(version);
 
 		setStatus("ready...");
